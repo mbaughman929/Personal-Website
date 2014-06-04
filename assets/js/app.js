5cbe9bdeab6643427c112e25ac93088964202c8c
Michael <mbaughman929@gmail.com>
2014-06-04 15:15:18 -0700

    fixed bugs in navbar, completed about section, and added more mobile functionality.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,11 @@
-
+//jQuery for page slow scrolling
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1600, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
 
