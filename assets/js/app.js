b3a3eed6d234b8c660fc0afc8b1d1c31f7cbafe6
Michael <mbaughman929@gmail.com>
2014-06-04 19:34:53 -0700

    completed contact section, added jquery to nav, page scroll, and intro text. Added custom Google Map, social media icons, and contact info.

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,5 +7,132 @@ $(function() {
         }, 1600, 'easeInOutExpo');
         event.preventDefault();
     });
+    //slow fade effect for body text
+    $(window).load(function(){
+		$('.intro-body').hide();
+		$('.intro .intro-body').fadeIn(4000);
+	});
+
+	//jQuery to collapse the navbar on scroll
+	$(window).scroll(function() {
+	    if ($(".navbar").offset().top > 50) {
+	        $(".navbar-fixed-top").addClass("top-nav-collapse");
+	    } else {
+	        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+	    }
+	});
 });
 
+// Google Map ref:snazzymaps.com
+var myOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(47.605384, -122.3355372),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "hue": "#F1FF00"
+            },
+            {
+                "saturation": -27.4
+            },
+            {
+                "lightness": 9.4
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "hue": "#0099FF"
+            },
+            {
+                "saturation": -20
+            },
+            {
+                "lightness": 36.4
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "stylers": [
+            {
+                "hue": "#00FF4F"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 0
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "hue": "#FFB300"
+            },
+            {
+                "saturation": -38
+            },
+            {
+                "lightness": 11.2
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "hue": "#00B6FF"
+            },
+            {
+                "saturation": 4.2
+            },
+            {
+                "lightness": -63.4
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "hue": "#9FFF00"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 0
+            },
+            {
+                "gamma": 1
+            }
+        ]
+    }
+]
+};
+
+
+var map = new google.maps.Map(document.getElementById('map'), myOptions);
